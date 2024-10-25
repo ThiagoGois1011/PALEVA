269495cb5e075e035e1e1e9669a7491f21f1d83d
Thiago Gois <thiagogoes1011@gmail.com>
2024-10-24 21:30:01 -0300

    Implementação: Método adiciona ao model opening_hour

--- app/models/opening_hour.rb
+++ app/models/opening_hour.rb
@@ -1,4 +1,19 @@
 class OpeningHour < ApplicationRecord
   belongs_to :establishment
   enum :day_of_week, {monday: 0, tuesday: 1, wednesday: 2, thursday: 3, friday: 4, saturday: 5, sunday: 6}
+
+  def self.closed?(day, hour)
+    horario_do_dia = OpeningHour.find_by(day_of_week: day) 
+    horario_de_abertura = horario_do_dia.open_hour.strftime("%H:%M")
+    horario_de_fechamento = horario_do_dia.close_hour.strftime("%H:%M")
+
+    puts hour
+    puts horario_de_abertura
+    puts horario_de_fechamento
+    if hour >= horario_de_abertura  && hour <= horario_de_fechamento
+      return false
+    end
+
+    true
+  end
 end
--- spec/models/opening_hour_spec.rb
+++ spec/models/opening_hour_spec.rb
@@ -1,5 +1,65 @@
 require 'rails_helper'
 
 RSpec.describe OpeningHour, type: :model do
-  
+  describe '#closed?' do 
+    it 'Mostra um comércio que ta fechado' do
+      user = User.create!(name: 'Andre', last_name: 'Silva Lopes', cpf: '44749124621', email: 'andre@email.com', password: 'password5498')
+      e = Establishment.create!(corporate_name: 'Ifood Distribuidora', brand_name: 'Ifood', restration_number: CNPJ.generate,
+                            full_address: 'Av Presidente Medice', phone_number: '11981546985', email: 'contato@ifood.com',
+                            code: '45668', user_id: user.id)
+      7.times do |day| 
+        OpeningHour.create!(establishment: e, open_hour: '08:00', 
+                            close_hour: '18:00', day_of_week: :monday)               
+      end
+
+      result = OpeningHour.closed?(:monday, '06:00')
+
+      expect(result).to be(true)
+    end
+
+    it 'Mostra um comércio que ta fechado. pt. 2' do
+      user = User.create!(name: 'Andre', last_name: 'Silva Lopes', cpf: '44749124621', email: 'andre@email.com', password: 'password5498')
+      e = Establishment.create!(corporate_name: 'Ifood Distribuidora', brand_name: 'Ifood', restration_number: CNPJ.generate,
+                            full_address: 'Av Presidente Medice', phone_number: '11981546985', email: 'contato@ifood.com',
+                            code: '45668', user_id: user.id)
+      7.times do |day| 
+        OpeningHour.create!(establishment: e, open_hour: '08:00', 
+                            close_hour: '18:00', day_of_week: :monday)               
+      end
+
+      result = OpeningHour.closed?(:monday, '19:00')
+
+      expect(result).to be(true)
+    end
+
+    it 'Mostra um comércio que ta aberto' do
+      user = User.create!(name: 'Andre', last_name: 'Silva Lopes', cpf: '44749124621', email: 'andre@email.com', password: 'password5498')
+      e = Establishment.create!(corporate_name: 'Ifood Distribuidora', brand_name: 'Ifood', restration_number: CNPJ.generate,
+                            full_address: 'Av Presidente Medice', phone_number: '11981546985', email: 'contato@ifood.com',
+                            code: '45668', user_id: user.id)
+      7.times do |day| 
+        OpeningHour.create!(establishment: e, open_hour: '08:00', 
+                            close_hour: '18:00', day_of_week: :monday)               
+      end
+
+      result = OpeningHour.closed?(:monday, '08:00')
+
+      expect(result).to be(false)
+    end
+
+    it 'Mostra um comércio que ta aberto. pt. 2' do
+      user = User.create!(name: 'Andre', last_name: 'Silva Lopes', cpf: '44749124621', email: 'andre@email.com', password: 'password5498')
+      e = Establishment.create!(corporate_name: 'Ifood Distribuidora', brand_name: 'Ifood', restration_number: CNPJ.generate,
+                            full_address: 'Av Presidente Medice', phone_number: '11981546985', email: 'contato@ifood.com',
+                            code: '45668', user_id: user.id)
+      7.times do |day| 
+        OpeningHour.create!(establishment: e, open_hour: '08:00', 
+                            close_hour: '18:00', day_of_week: :monday)               
+      end
+
+      result = OpeningHour.closed?(:monday, '16:00')
+
+      expect(result).to be(false)
+    end
+  end
 end
